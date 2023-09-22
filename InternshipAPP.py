9447f5125098eb35d33410710249a29d2503e63c
Cecilia0920 <143489308+Cecilia0920@users.noreply.github.com>
2023-09-22 10:44:31 +0800

    Update InternshipAPP.py

--- InternshipAPP.py
+++ InternshipAPP.py
@@ -200,7 +200,9 @@ def loginLecturer():
         return render_template('studentList.html')
     else:
         error_message='Login failed! Invalid email or password.'
-        return error_message
+        return render_template('lecturer-login.html', error_message=error_message)
+
+    return render_template('lecturer-login.html', error_message=error_message)
 
 @app.route("/studentList.html")
 def studentDashboard():
